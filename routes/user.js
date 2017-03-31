04350b3995759d4371f4443a74efc1927b04e6c7
ljzzkkkss <793958447@qq.com>
2017-04-01 02:07:52 +0800

    登陆调整

--- routes/user.js
+++ routes/user.js
@@ -48,20 +48,49 @@ router.post('/login', function(req, res, next) {
 
 router.post('/checkLogin', function(req, res, next) {
   var param = req.body;
-  var result;
-  if (param.sessionid == cacheUtil.get(param.username + '_sessonid') && param.token == cacheUtil.get(param.username + '_token')){
-    var token = util.md5(util.md5(util.randomnumber(8)));
-    cacheUtil.set(param.username + '_token', token);
-    result = {
-      success: true,
-      token: token
+  cacheUtil.get(param.username + '_sessonid',function (err, response){
+    var result;
+
+    console.log('checkLogin-sessonid-err',err);
+    console.log('checkLogin-sessonid-response',response);
+
+    if(response != null && response == param.sessionid){
+
+      cacheUtil.get(param.username + '_token',function (err, response){
+
+        console.log('checkLogin-token-err',err);
+        console.log('checkLogin-token-response',response);
+
+        if(response != null && response == param.token){
+
+          var token = util.md5(util.md5(util.randomnumber(8)));
+          cacheUtil.set(param.username + '_token', token);
+
+          if(param.rememberme) {
+            cacheUtil.expire(param.username + '_sessonid', 3600 * 24 * 7);
+          }else{
+            cacheUtil.expire(param.username + '_sessonid', 300);
+          }
+
+          result = {
+            success: true,
+            token: token
+          };
+          jsonUtil.write(res,result);
+        }else{
+          result={
+            success: false
+          };
+          jsonUtil.write(res,result);
+        }
+      });
+    }else{
+      result={
+        success: false
+      };
+      jsonUtil.write(res,result);
     }
-  }else{
-    result = {
-      success: false
-    }
-  }
-  jsonUtil.write(res,result);
+  });
 });
 
 router.post('/logout', function(req, res, next) {
@@ -72,6 +101,14 @@ router.post('/logout', function(req, res, next) {
     success: true
   };
   jsonUtil.write(res,result);
+});
+
+router.post('/queryByUsername', function(req, res, next) {
+  var param = req.body;
+  var queryByUserName = function (result) {
+    jsonUtil.write(res,result);
+  };
+  userDao.queryByUsername(param,queryByUserName);
 });
 
 
